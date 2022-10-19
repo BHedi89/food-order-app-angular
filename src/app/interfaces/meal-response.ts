7819c505147e70473cc5fc8e4d12b84bb61ed1b0
BHedi89 <hedi.besenyei89@gmail.com>
2022-10-19 11:56:43 +0200

    delete uncessary interfaces

--- src/app/interfaces/meal-response.ts
+++
@@ -1,9 +0,0 @@
-import { Meals } from "./meals";
-
-export interface MealResponse {
-    id: {
-        name: string,
-        description: string,
-        price: number
-    }
-}
--- src/app/interfaces/meals.ts
+++
@@ -1,6 +0,0 @@
-export interface Meals {
-    id: string,
-    name: string,
-    description: string,
-    price: number
-}
