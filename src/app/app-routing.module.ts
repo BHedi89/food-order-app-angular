deece5ef8d31eac7cd53b4ecd16a8b0c8d300b57
BHedi89 <hedi.besenyei89@gmail.com>
2022-10-19 13:51:37 +0200

    add router path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MealsPageComponent } from './components/meals/meals-page/meals-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'meals'},
+  {path: 'meals', component: MealsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
